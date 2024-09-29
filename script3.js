87c55d16eb5f1e3cbb53b84722ff0643252b67b6
Héric Moura <111160949+hericmr@users.noreply.github.com>
2024-09-29 10:48:16 -0300

    Update script3.js

--- script3.js
+++ script3.js
@@ -35,28 +35,28 @@ function criarVideoItem(professor) {
     }
 
     // Verifica se 'aldeia' está presente
-    const pAldeia = document.createElement('p');
-    if (professor.aldeia) {
-        pAldeia.innerHTML = `<strong>Aldeia:</strong> ${professor.aldeia}`;
-    } else {
-        pAldeia.innerHTML = `<strong>Aldeia:</strong> Não Informado`;
-    }
+    //const pAldeia = document.createElement('p');
+    //if (professor.aldeia) {
+    //    pAldeia.innerHTML = `<strong>Aldeia:</strong> ${professor.aldeia}`;
+    //} else {
+    //    pAldeia.innerHTML = `<strong>Aldeia:</strong> Não Informado`;
+    //}
 
     // Verifica se 'povo' está presente
-    const pPovo = document.createElement('p');
-    if (professor.povo) {
-        pPovo.innerHTML = `<strong>Povo:</strong> ${professor.povo}`;
-    } else {
-        pPovo.innerHTML = `<strong>Povo:</strong> Não Informado`;
-    }
+    //const pPovo = document.createElement('p');
+    //if (professor.povo) {
+    //    pPovo.innerHTML = `<strong>Povo:</strong> ${professor.povo}`;
+    //} else {
+    //    pPovo.innerHTML = `<strong>Povo:</strong> Não Informado`;
+    //}
 
     // Verifica se 'uf' está presente
 
 
     // Adiciona os parágrafos à div de detalhes
     professorDetails.appendChild(pNome);
-    professorDetails.appendChild(pAldeia);
-    professorDetails.appendChild(pPovo);
+    //professorDetails.appendChild(pAldeia);
+    //professorDetails.appendChild(pPovo);
     
 
     // Adiciona a imagem e os detalhes à seção de informações
